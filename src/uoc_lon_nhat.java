b690e7d7066881c26a237d3e962ff99c712e25d6
Cao Thien <xxk30a6as2@gmail.com>
2022-04-04 14:46:42 +0700

    add uoc max

---
+++ src/uoc_lon_nhat.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class uoc_lon_nhat {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap a: ");
+        a = sc.nextInt();
+        System.out.println("nhap b: ");
+        b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("khong co uong chung lon nhat: ");
+        }
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println("uoc lo nhat la: " + a);
+    }
+}
