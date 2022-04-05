09e039c89763bf38c37e3caa83deb03e689fa538
Cao Thien <xxk30a6as2@gmail.com>
2022-04-05 23:12:25 +0700

    add giatritrongmang

---
+++ src/array/tim_gia_tri_trong_mang.java
@@ -0,0 +1,22 @@
+package array;
+import java.util.Scanner;
+
+public class tim_gia_tri_trong_mang {
+    public static void main(String[] args) {
+        String[] studens = {"thien", "son", "hunw", "huy", "nam"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap vao kiem tra ten: ");
+        String ip_name = sc.nextLine();
+        boolean isExist = false;
+        for (int i = 0; i < studens.length; i++) {
+            if (studens[i].equals(ip_name)) {
+                System.out.println("ten hoc sinh co trong lop: " + ip_name + " " + (i + 1)) ;
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            System.out.println(ip_name + " khong co trong mang");
+        }
+    }
+}
