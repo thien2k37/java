53c59e02aa6c9bf0ce159886fdd860523f44b13a
Cao Thien <xxk30a6as2@gmail.com>
2022-04-05 23:31:16 +0700

    add max arr

---
+++ src/array/max_arr.java
@@ -0,0 +1,19 @@
+package array;
+import java.util.Scanner;
+
+
+public class max_arr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("nhap vao pt thu " + (i + 1) + " la: ");
+            arr[i] = sc.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Số lớn nhất trong mang là: " + max);
+    }
+}
