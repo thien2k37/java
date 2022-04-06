9c9d243e9d127798ec0d0e318b7f1e266b9e5812
Cao Thien <xxk30a6as2@gmail.com>
2022-04-06 09:36:02 +0700

    add all

---
+++ src/array/delete_for_arr.java
@@ -0,0 +1,45 @@
+package array;
+import java.util.Scanner;
+
+
+public class delete_for_arr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("nhap pt vao mang; ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("nhap tu can xoa");
+        int x = sc.nextInt();
+
+        int viTriCanXoa = viTriPT(arr,x);
+
+        int[] newArr = new int[arr.length - 1];
+        for (int i = 0; i < viTriCanXoa; i++) {
+            newArr[i] = arr[i];
+        }
+        for (int i = viTriCanXoa; i < arr.length - 1; i++) {
+            newArr[i] = arr[i + 1];
+        }
+        arr = newArr;
+        inMang(arr);
+
+    }
+    static int viTriPT(int[] mang, int socantim) {
+        int viTri = -1;
+        for (int i = 0; i < mang.length; i++) {
+            if (mang[i] == socantim) {
+                viTri = i;
+            }
+        }
+        return viTri;
+    }
+
+    static void inMang(int[] mang) {
+        System.out.println("Mảng là: ");
+        for (int i = 0; i < mang.length; i++) {
+            System.out.print(mang[i]+"\t");
+        }
+    }
+}
--- src/array/mix_arr.java
+++ src/array/mix_arr.java
@@ -1,2 +1,23 @@
-package array;public class mix_arr {
+package array;
+import java.util.Scanner;
+
+public class mix_arr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("nhap cac pt thu " + (i + 1));
+            arr[i] = sc.nextInt();
+        }
+        System.out.println( "so be nhat trong mang la: " + min(arr));
+    }
+    public static int min(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
 }
